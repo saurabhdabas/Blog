4262e9c1e9f7d57221d844770cfb8954f487ecac
Saurabh Dabas <developerdabas@gmail.com>
2022-06-01 01:55:21 +0530

    Change route paths

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Navbar from './components/Navbar';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Home from './pages/Home';
 import CreatePost from './pages/CreatePost';
 import Login from './pages/Login';
+
+
 function App() {
+  
   return (
     <Router>
-      <Navbar/>
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/home" element={<Home/>}/>
         <Route path="/create" element={<CreatePost/>}/>
-        <Route path="/login" element={<Login/>}/>
       </Routes>
     </Router>
   );
