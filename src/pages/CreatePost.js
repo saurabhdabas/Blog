3afbbb527b981f00e0ddeda95fad6d40b3091f4a
Saurabh Dabas <developerdabas@gmail.com>
2022-05-31 11:57:41 +0530

    Add home, Login and createpost page

---
+++ src/pages/CreatePost.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreatePost = () => {
+  return (
+    <div>
+      I'm Create Post page.
+    </div>
+  )
+}
+
+export default CreatePost
---
+++ src/pages/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      I'm Login Page.
+    </div>
+  )
+}
+
+export default Login
