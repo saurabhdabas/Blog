a915ea78f7c3dbe6a46bc27f0ff8aa0a0502a473
Saurabh Dabas <developerdabas@gmail.com>
2022-06-01 01:52:15 +0530

    Fix the select account options issue

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -19,3 +19,6 @@ const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: 'select_account'
+});
