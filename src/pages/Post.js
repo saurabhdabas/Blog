ab6d242afe10c6901581ca26f60d4c4b382d9438
Saurabh Dabas <developerdabas@gmail.com>
2022-06-02 23:22:58 +0530

    Fix post component

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,20 +1,38 @@
-import { React } from 'react';
+import { React,useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import { Grid, Box, Typography, Avatar, Chip } from  '@mui/material';
 import Navbar from '../components/Navbar';
-import today from '../helpers/DateConversion';
+import { getDocs, collection } from 'firebase/firestore';
+import { db } from '../firebase-config';
+
 
 const Post = () => {
+
+  const [postsList, setPostsList] = useState([]);
+  const postsCollectionRef = collection( db, "posts");
+
+  useEffect(()=>{
+    getDocs(postsCollectionRef).then((res)=>{
+      setPostsList(res.docs.map((doc)=>({...doc.data(), id: doc.id})));
+    })
+  },[])
+ 
   // Retrieving user Info from local Storage
-  const user = JSON.parse(localStorage.getItem('user'));;
-  return (
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  const { id } = useParams();
+ 
+  const posts = postsList.map((post)=> {
+    if(post.id === id) {
+      console.log("article:",post)
+       return (
     <div>
-      <Navbar/>
       <Grid
         container
         spacing={0}
         direction="column"
         alignItems="center"
-        justifyContent="center"
+        justifyContent="start"
         style={{ minHeight: '100vh', backgroundColor: "#F1F3F4" }}
       >
         <Box
@@ -25,24 +43,22 @@ const Post = () => {
           padding={3}
           justifyContent="space-around"
           borderRadius={5}
-          sx={{width:1000, marginTop:15, marginBottom:10}}
-          noValidate
-          autoComplete="off"
+          sx={{width:1000, marginTop:15, marginBottom:15}}
         >
-          <Typography variant="h6" noWrap component="div" fontSize={26} fontFamily="'Raleway', sans-serif">
-            
+          <Typography variant="h6" noWrap component="div" fontSize={26}        fontFamily="'Raleway', sans-serif">
+            {post.title}
           </Typography>
           
           <Box display='flex' flexDirection="row" alignItems='center' justifyContent='space-between' sx={{width:900, marginTop:5, marginBottom:5}}>
             <Typography variant="h6" noWrap component="div" fontSize={16}   fontFamily="'Raleway', sans-serif">
-              Published on : {today}
+              Published on : {post.publishDate}
             </Typography>
             <Chip
-              avatar={<Avatar alt={user.name} src={user.photo} />}
+              avatar={<Avatar alt={post.author.name} src={user.photo} />}
               label=
               {              
                 <Typography variant="h6" noWrap component="div" fontSize={16} fontFamily="'Raleway', sans-serif">
-                {user.name}
+                {post.author.name}
                 </Typography>
               }
               variant="outlined" 
@@ -56,17 +72,23 @@ const Post = () => {
             fontFamily="'Raleway', sans-serif"
             sx={{width:900, marginTop:5, marginBottom:5}}
           >
-            What is Lorem Ipsum?
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            What is Lorem Ipsum?
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.            What is Lorem Ipsum?
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.            What is Lorem Ipsum?
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.            
+            {post.content}            
           </Box>
         </Box>
       </Grid>
     </div>
   )
+    }
+  });
+  return (
+    <Grid style={{ minHeight: '100vh', backgroundColor: "#F1F3F4" }}>
+      <Navbar/>
+      <Box>
+        {posts}
+      </Box>
+    </Grid>
+  )
+
 }
 
 export default Post;
